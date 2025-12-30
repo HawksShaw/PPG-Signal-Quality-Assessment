4f8a6762fcb4688b78ec82c1f391ff733c15d85a
Robert Wyrobek <robik272003@gmail.com>
2025-12-30 19:36:18 +0100

    Start validation pt.2

--- scripts/ml_validation.py
+++ scripts/ml_validation.py
@@ -6,3 +6,9 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import sqlite3
 import json
+
+db_path = "quality_assessment.db"
+
+def load_data():
+    connect = sqlite3.connect(db_path)
+    
